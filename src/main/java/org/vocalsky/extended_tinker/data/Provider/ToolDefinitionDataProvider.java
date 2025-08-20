3e1768b898c81e3075eb33dc5c6c64a92e151f40
VocalSky <LQ_636721@outlook.com>
2025-08-21 07:38:50 +0800

    修改 DragonArmor 的 definition

--- src/main/java/org/vocalsky/extended_tinker/data/Provider/ToolDefinitionDataProvider.java
+++ src/main/java/org/vocalsky/extended_tinker/data/Provider/ToolDefinitionDataProvider.java
@@ -85,9 +85,7 @@ public class ToolDefinitionDataProvider extends AbstractToolDefinitionDataProvid
 
         for (ItemDragonArmor.DragonArmorType type : ItemDragonArmor.DragonArmorType.values())
             defineArmor(IafToolDefinitions.DRAGON_ARMOR_MATERIAL.get(type))
-            .module(slots -> MaterialStatsModule.stats().stat(DragonArmorMaterialStats.stats.get(type)).build())
-//            .module(slots -> MaterialStatsModule.stats().stat(PlatingMaterialStats.TYPES.get(slots.ordinal())).build())
-//            .module(slots -> MaterialStatsModule.stats().stat(DragonPlatingMaterialStats.TYPES.get(slots.ordinal())).build())
+            .module(slots -> MaterialStatsModule.stats().stat(DragonArmorMaterialStats.stats.get(type)).stat(PlatingMaterialStats.TYPES.get(slots.ordinal())).build())
             .module(DefaultMaterialsModule.builder().material(MaterialIds.cobalt).material(MaterialIds.ancientHide).build())
             .module(new MultiplyStatsModule(MultiplierNBT.builder().set(ToolStats.DURABILITY, 5.0f).set(ToolStats.ARMOR, 2.5f).set(ToolStats.ARMOR_TOUGHNESS, 1.75f).set(ToolStats.KNOCKBACK_RESISTANCE, 1.5f).build()))
             .module(ToolSlotsModule.builder().slots(SlotType.ABILITY, 1).slots(SlotType.UPGRADE, 3).slots(SlotType.DEFENSE, 1).build());
